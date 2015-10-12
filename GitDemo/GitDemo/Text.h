c6fdbd2c67f4796641f3c8a9fe9099eeadb17427
qianfeng <846947300@qq.com>
2015-10-12 11:45:05 +0800

    Text

---
+++ GitDemo/GitDemo/Text.h
@@ -0,0 +1,13 @@
+//
+//  Text.h
+//  GitDemo
+//
+//  Created by qianfeng on 15/10/12.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Text : UIView
+
+@end
---
+++ GitDemo/GitDemo/Text.m
@@ -0,0 +1,21 @@
+//
+//  Text.m
+//  GitDemo
+//
+//  Created by qianfeng on 15/10/12.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import "Text.h"
+
+@implementation Text
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
